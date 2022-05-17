d010775815891e9c6b61faafbaf08e0e5c6b9766
Orkhan V. Gahramanli <orkhanvg@code.local>
2022-05-17 16:52:51 +0400

    feat:js1

---
+++ script.js
@@ -0,0 +1,83 @@
+// Task1
+
+function divide(num1, num2)
+{
+return  num1/num2;
+}
+
+console.log(divide(50,5));
+
+// Task2
+
+function Repeat (word, count)
+{
+    result=" "
+  for (let index = 0; index < count; index++) {
+      result+=word;
+      
+  }
+  return result;
+
+}
+console.log(Repeat("Ha",30));
+
+
+
+
+// Task3.1
+
+function Method(R)
+
+{
+
+return R*R*3
+    
+}
+console.log(Method(39))
+
+
+
+
+// Task3.2/////
+function Method5(a,b) 
+{
+    return a*b;
+}
+
+console.log(Method5(423,8484))
+
+
+
+
+
+
+
+
+
+
+
+// Task3.3//////
+
+function Method2 (a,b) 
+{
+    return a*b;
+}
+
+console.log(Method2(30,77))
+
+// Task3.3////////
+
+function Method3(p,r)
+{
+    return p*r;
+    
+}
+console.log (Method3(49,79))
+
+function Method3_1(a,b,c)
+{
+ 
+    return a+b+c/2;
+}
+
+console.log (Method3_1(30,55,93))
